623597eec8b01768d127ff750f2093d83b0182ca
kyr4601 <jane4545@naver.com>
2025-05-12 17:17:39 +0900

    feat: 유저 도메인

---
+++ src/main/java/com/fortune/fortune/domain/User.java
@@ -0,0 +1,41 @@
+package com.fortune.fortune.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @Column(name = "id", updatable = false, insertable = false)
+    private UUID id;
+
+    private String nickname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "birth_time")
+    private LocalTime birthTime;
+
+    private String gender;
+
+    /*@AllArgsConstructor lombok의 해당 어노테이션으로 대체 가능함
+    public User(UUID id, String nickname, LocalDate birthDate, LocalTime birthTime, String gender) {
+        this.id = id;
+        this.nickname = nickname;
+        this.birthDate = birthDate;
+        this.birthTime = birthTime;
+        this.gender = gender;
+    }*/
+}
