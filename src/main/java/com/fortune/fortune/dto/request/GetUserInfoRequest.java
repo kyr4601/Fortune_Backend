27ef6c35eea7746a860a51baf09407841ed45a9d
kyr4601 <jane4545@naver.com>
2025-05-14 02:38:34 +0900

    feat: 유저 닉네임 조회 dto 생성

---
+++ src/main/java/com/fortune/fortune/dto/request/GetUserInfoRequest.java
@@ -0,0 +1,6 @@
+package com.fortune.fortune.dto.request;
+
+import java.util.UUID;
+
+public record GetUserInfoRequest(UUID id) {
+}
---
+++ src/main/java/com/fortune/fortune/dto/response/UserData.java
@@ -0,0 +1,4 @@
+package com.fortune.fortune.dto.response;
+
+public record UserData(String nickname) {
+}
