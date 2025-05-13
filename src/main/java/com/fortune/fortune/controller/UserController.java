92b4242f57d33ed5a260760a33b957b11b8118aa
kyr4601 <jane4545@naver.com>
2025-05-14 02:06:46 +0900

    feat: 유저 닉네임 정보 조회 테스트용 api 생성

--- src/main/java/com/fortune/fortune/controller/UserController.java
+++ src/main/java/com/fortune/fortune/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.fortune.fortune.controller;
 
+import com.fortune.fortune.common.response.BaseResponse;
+import com.fortune.fortune.domain.User;
+import com.fortune.fortune.dto.response.UserData;
+import com.fortune.fortune.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping ("/nickname")
+    public BaseResponse<UserData> getUserNickname(@RequestParam UUID id){
+        User user = userService.findUser(id);
+        UserData response = new UserData(user.getNickname());
+        return BaseResponse.success(response);
+    }
 }
